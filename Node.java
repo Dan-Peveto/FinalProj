1f1520b147b0a2d8cc9bbd23ac50667141abe9f3
dan-peveto <daniel_peveto@outlook.com>
2023-12-08 22:24:21 -0800

    added insert and commit methods

--- Node.java
+++ Node.java
@@ -1,9 +1,9 @@
 public class Node {
     // field
     private int value;
-    private int left, right, parent; 
-
-    boolean color;
+    private int left;
+    private int right, parent; 
+    private int color; 
     // method
 
     // CTOR
--- RBT.java
+++ RBT.java
@@ -1,9 +1,17 @@
 public class RBT {
     // Fields
-    
-    
+    private Node root; // Create root node
+    private Node TNULL; // presents the NIL leaves
     // Methods
 
+    // Preorder Method
+    private void preOrderHelper(Node node) 
+    {
+        if (node != TNULL) // If not null keep going
+            {   preOrderHelper(node.left);
+                preOrderHelper(node.right);
+            }
+    }
     // insertion / Print -- Insert Print
     // balance after insert / rotate left / rotate right -- Trevor
     // get height / delete -- Dan
